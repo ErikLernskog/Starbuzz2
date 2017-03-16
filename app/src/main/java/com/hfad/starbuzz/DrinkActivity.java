470dae34d6bac150e30b07a5cdd50f148c5edde4
ErikLernskog <funbo@hotmail.com>
2017-03-16 08:25:05 +0100

    Async task for db done

--- app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -14,7 +14,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
-
 public class DrinkActivity extends Activity {
     public static final String EXTRA_DRINKNO="drinkNo";
     @Override
